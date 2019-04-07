8f05fd74fc36c6227aedfa4d86f913a2a908b15b
jakinpilla <jakinpilla@gmail.com>
2019-04-07 23:13:24 +0900

    updated

--- ADP_text_mining_2.R
+++ ADP_text_mining_2.R
@@ -2,114 +2,169 @@ setwd("C:/Users/Daniel/ADP_performance_test")
 getwd()
 
 # loading needed packages----
-Packages <- c('plyr', 'dplyr', 'tidyverse', 'data.table', 'reshape2', 'caret', 'rpart', 'GGally', 'ROCR', 'party', 
-              'randomForest', 'dummies', 'curl', 'gridExtra')
+Packages <- c('plyr', 'dplyr', 'tidyverse', 'data.table', 'reshape2', 'caret', 'rpart', 'GGally', 'ROCR', 'party', 'randomForest', 'dummies', 'curl', 'gridExtra')
 
 Packages_tm <- c('tm', 'rJava', 'KoNLP', 'SnowballC', 'slam', 'wordcloud')
 
 lapply(Packages, library, character.only=T)
 lapply(Packages_tm, library, character.only=T); useSejongDic()
 
-options(mc.cores=1) # not multi_core
-
 # loading text data----
 tvpro <- read_delim("data/tvprograms.txt", "\t", escape_double = FALSE, trim_ws = TRUE)
-head(tvpro$contents, 10)
-
-# Corpus----
-cps <- VCorpus(VectorSource(tvpro$contents))
-cps <- tm_map(cps, removePunctuation) # 구두점 제거
-cps <- tm_map(cps, stripWhitespace) # 2개 이상 빈칸을 하나로
-cps <- tm_map(cps, removeWords,
-              c('mbs', 'kbs', 'sbs', '2016년도'))
+head(tvpro$contents, 30)
 
 # 명사 빈도 분석----
-## ko.words() :: 명사 추출 함수 정의----
-ko.words <- function(doc) {
-  d <- as.character(doc)
-  extractNoun(d)
+contents.vec <- tvpro$contents
+contents.vec[is.na(contents.vec)] # contents에는 NA 값 없음
+
+# 불용문자 제거 ----
+contents_doc.vec <- gsub('[[:punct:]]+', "", contents_doc.vec)
+contents_doc.vec <- gsub('[[:lower:]]+', "", contents_doc.vec)
+contents_doc.vec <- gsub("n", "", contents_doc.vec)
+contents_doc.vec <- gsub("N", "", contents_doc.vec)
+contents_doc.vec <- gsub("<U+00A0>", "", contents_doc.vec)
+contents_doc.vec <- gsub("00A0", "", contents_doc.vec)
+contents_doc.vec <- gsub("▲", "", contents_doc.vec)
+contents_doc.vec <- gsub("▼", "", contents_doc.vec)
+contents_doc.vec <- gsub("<U+00A0>", "", contents_doc.vec)
+
+contents.vec[is.na(contents.vec)]
+
+head(contents_doc.vec, 30)
+
+contents_doc.vec <- NULL
+for (i in 1:length(contents.vec)) {
+  
+  contents.vec[i] %>% # tagging... 
+    SimplePos09() %>% 
+    unlist() %>% 
+    unname() %>% 
+    as.character() %>%
+    strsplit("\\+") %>% 
+    unlist() -> ith.tagged
+  
+  ith.tagged[grep("/N", ith.tagged)] %>% # extract only Nouns...
+    paste(collapse = " ") -> ith.N_tagged
+  
+  contents_doc.vec <- c(contents_doc.vec, ith.N_tagged)
 }
 
-## TermDocumentMatrix----
-tdm_contents <- TermDocumentMatrix(cps, 
-                                   control = list(tokenize = ko.words,
+contents_doc.vec %>% length()
+contents_doc.vec <- gsub('[[:punct:]]+', "", contents_doc.vec)
+contents_doc.vec <- gsub('[[:lower:]]+', "", contents_doc.vec)
+contents_doc.vec <- gsub("n", "", contents_doc.vec)
+contents_doc.vec <- gsub("N", "", contents_doc.vec)
+contents_doc.vec <- gsub("■", "", contents_doc.vec)
+contents_doc.vec <- gsub("△", "", contents_doc.vec)
+contents_doc.vec <- gsub("▲", "", contents_doc.vec)
+contents_doc.vec <- gsub("▷", "", contents_doc.vec)
+contents_doc.vec <- gsub("▼", "", contents_doc.vec)
+contents_doc.vec <- gsub("◇", "", contents_doc.vec)
+contents_doc.vec <- gsub("◆", "", contents_doc.vec)
+contents_doc.vec <- gsub("00a0", "", contents_doc.vec)
+contents_doc.vec <- gsub("★", "", contents_doc.vec)
+
+# DTM 생성----
+strsplit_space_tokenizer <- function(x) unlist(strsplit(as.character(x), " "))
+contents_doc.vec[1] %>% strsplit_space_tokenizer()
+
+options(mc.cores=1)
+cps <- VCorpus(VectorSource(contents_doc.vec))
+dtm_contents <- DocumentTermMatrix(cps, 
+                                   control = list(tokenize = strsplit_space_tokenizer,
                                                   removePunctuation=T,
                                                   wordLengths=c(2, 6),
-                                                  weighting = weightBin))
-# TDM inspect----
-# TDM 일부내용 확인하여 보기(inspect())----
-inspect(tdm_contents[1:10, 1:10])
+                                                  weighting = weightTfIdf)
 
-# TDM에서 빈출어휘 살펴보기(findAssocs())----
-findAssocs(tdm_contents, "1박2일", .2)
-findAssocs(tdm_contents, "복면가왕", .2)
+# DTM inspect----
+tm::inspect(dtm_contents[1:50, ])
+dtm_contents %>% colnames() -> terms.of_dtm
 
-tdm_contents_mat <- as.matrix(tdm_contents)
-tdm_rowsum <- rowSums(tdm_contents_mat)
-tdm_rowsum_desc <- tdm_rowsum[order(tdm_rowsum, decreasing=T)]; tdm_rowsum_desc
+# 연관어휘(findAssocs())----
+findAssocs(dtm_contents, "1박2일", .2)
+findAssocs(dtm_contents, "복면가왕", .2)
 
-#  TDM에서 바로 합을 구하기
-library(slam)
-as.data.frame(row_sums(tdm_contents, na.rm=T)) -> wd_count_df; head(wd_count_df)
-data.frame(words = rownames(wd_count_df), freq = wd_count_df$`row_sums(tdm_contents, na.rm = T)`) -> wd_count_df
-wd_count_df %>%
-  colnames()
-wd_count_df %>%
-  arrange(desc(freq)) -> wd_count_df ; wd_count_df %>% head(100)
+# bigmemory for dtm ----
+
+# install.packages('bigmemory')
+library(bigmemory)
+options(bigmemory.allow.dimnames = T)
+contents_big_mat <- as.big.matrix(as.matrix(dtm_contents)) 
+length(terms.of_dtm)
+dim(contents_big_mat)
+
+colnames(contents_big_mat) <- terms.of_dtm
+
+contents_big_mat[1, ] # encoding 문제 발생...극복방안은??
+
+dtm_col_sum <- colSums(contents_big_mat)
+dtm_col_sum_desc <- dtm_col_sum[order(dtm_col_sum, decreasing=T)]; dtm_col_sum_desc[1:30]
+
+#  DTM에서 slam 이용한 wordcount----
+#  library(slam)
+
+dtm_contents %>%
+  slam::col_sums() %>%
+  base::as.data.frame() %>%
+  tibble::rownames_to_column() %>%
+  as_tibble() %>%
+  rename(words = rowname, freq = ".") %>%
+  arrange(desc(freq))
 
 # 문서 유사도 구하기----
-as.matrix(tdm_contents) -> tdm_contents_mat; dim(tdm_contents_mat)
-n <- nrow(tdm_contents_mat)
+as.matrix(dtm_contents) -> dtm_contents_mat; dim(dtm_contents_mat)
+n <- nrow(dtm_contents_mat)
 idx <- 1:n
 sample_idx <- sample(idx, n * .2) # 20%만 문서 무작위 선택
-sample_tdm_mat = tdm_contents_mat[sample_idx, ]
-dim(sample_tdm_mat)
+sample_dtm_mat = dtm_contents_mat[sample_idx, ]
+dim(sample_dtm_mat)
+sample_dtm_mat[1:10, ] %>% rownames()
+sample_idx[1:10]
 
-# norm_vec()----
-norm_vec <- function(x) {
-  return((x-min(x)) / (max(x) - min(x)))} # 정규화 함수 정의
 
-# normalize----
-tdm_sample_mat <- apply(sample_tdm_mat, 2, norm_vec)
-sample_tdm_mat[1:10, 1:10]
+# dtm_sample_mat 의 9번째 문서와 1~8번째 문서와의 유사도 계산
 
-# tdm_sample_mat 의 9번째 문서와 1~8번째 문서와의 유사도 계산
-cord <- t(tdm_sample_mat[, 9]) %*% tdm_sample_mat[, 1:8]
+# dtm_sample_mat 의 9번째 문서 번호...
+sample_idx[9]
+
+cord <- sample_dtm_mat[9, ] %*% t(sample_dtm_mat)
 cord
 
 # 문서 유사도순으로 배열하기----
-orders <- data.frame(docs = 'doc_9', scores = t(cord), stringsAsFactors = F); orders
+orders <- data.frame(docs = 'doc_347', scores = t(cord), stringsAsFactors = F); orders
 orders %>%
-  arrange(desc(scores))
+  as_tibble() %>%
+  rownames_to_column() %>%
+  rename(doc.num = rowname) %>%
+  arrange(desc(scores)) -> similarity.for_doc_347
+
+contents.vec[as.numeric(similarity.for_doc_347$doc.num[1:10])]
 
 # 문서 군집하기----
-n <- nrow(tdm_contents_mat)
+n <- nrow(dtm_contents_mat)
 sample_idx <- sample(idx, n * .05)
-tdm_sample_mat <- tdm_contents_mat[sample_idx, ]; dim(tdm_sample_mat)
-tdm_sample_mat <- tdm_sample_mat[1:200, 1:100]; dim(tdm_sample_mat)# 계산 시간 다대하여 데이터를 축소함.
+dtm_sample_mat <- dtm_contents_mat[sample_idx, ]; dim(dtm_sample_mat)
+dtm_sample_mat <- dtm_sample_mat[1:200, ]; dim(dtm_sample_mat)# 계산 시간 다대하여 데이터를 축소함.
 
-fit <- hclust(dist(t(tdm_sample_mat)), method='ward.D2')
-plclust(fit)
+fit <- hclust(dist(dtm_sample_mat), method='ward.D2')
+plot(fit)
 rect.hclust(fit, k=5)
 
-# matrix 변환, 행의 합, 빈도 정렬 및 wordcloud----
+# wordcount 및 wordcloud----
+dtm_contents %>%
+  slam::col_sums() %>%
+  base::as.data.frame() %>%
+  tibble::rownames_to_column() %>%
+  as_tibble() %>%
+  rename(words = rowname, freq = ".") %>%
+  arrange(desc(freq)) -> wordcnt.data
 
-## matrix 변환----
-as.matrix(tdm_contents) -> tdmat; dim(tdmat)
-
-## 행의 합----
-rowSums(tdmat) -> tdmat_sum
-
-## 빈도 정렬
-tdmat_sum[order(tdmat_sum, decreasing = T)] -> tdmat_arr; tdmat_arr[1:10]
-
-## wordcloud----
-as.data.frame(tdmat_arr) -> df; head(df)
+# wordcloud....
 windowsFonts()
 windowsFonts(malgun=windowsFont("맑은 고딕"))
-wordcloud(words = rownames(df), freq=df$tdmat_arr, 
-          min.freq= 5, 
-          max.words = 100,
+wordcloud(words = wordcnt.data$words, freq=wordcnt.data$freq,
+          min.freq= 50, 
+          max.words = 300,
           random.order = F,
           family = "malgun", 
           colors=brewer.pal(8, "Dark2"))
