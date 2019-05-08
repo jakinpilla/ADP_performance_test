fa1c73fa1f2442db003888f111f512c8078b9cfa
jakinpilla <jakinpilla@gmail.com>
2019-05-08 20:19:08 +0900

    updated

--- ADP_r_basic_2.R
+++ ADP_r_basic_2.R
@@ -17,7 +17,7 @@ Packages <- c('tidyverse', 'data.table', 'reshape2', 'caret', 'rpart', 'GGally',
 
 lapply(Packages, library, character.only=T)
 
-# Binning----
+# Binning ----------------------------------------------------------------------
 library(mfp)
 data(bodyfat)
 glimpse(bodyfat);
@@ -30,27 +30,28 @@ bodyfat$bmi.bins <- cut(bodyfat$bmi,
 bodyfat
 glimpse(bodyfat); levels(bodyfat$bmi.bins)
 
-# Time binning ----
+# Time binning -----------------------------------------------------------------
 tran <- read_csv('./data/transaction.csv'); tran %>% colnames()
 rename(tran, hour = time) -> tran #
 tran$hour <- as.numeric(substr(tran$hour, 1, 2))
 
 tran %>% mutate(h_bin = cut(hour, 
                             breaks = c(0, 6, 12, 18, 23),
-                            include.lowest = T, # 0을 그룹에 포함....
+                            include.lowest = T, # to include 0 in a group...
                             labels=c('0-5', '6-11', '12-17', '18-23'))) -> tran
 head(tran)
-unique(tran$h_bin) # '0-5' 시간대가 없음에 유의
+unique(tran$h_bin) # There is no '0-5' time bin...
 
-# h_bin one-hot coding----
+# h_bin one-hot coding ---------------------------------------------------------
 tran %>%
   select(custid, h_bin, amt) %>%
   group_by(custid, h_bin) %>%
   summarise(sum.amt = sum(amt)) %>%
+  ungroup() %>%
   spread(h_bin, sum.amt, fill = 0) %>%
   mutate_if(is.numeric, funs(replace(., . > 0, 1))) # to dummies... that's it!
 
-#' h_bin freq ratio ----
+#' h_bin freq ratio ------------------------------------------------------------
 tran %>%
   select(custid, h_bin, amt) %>%
   group_by(custid, h_bin) %>%
@@ -65,7 +66,7 @@ tran %>%
   mutate(total_freq = rowSums(select_if(., is.numeric))) %>%
   mutate_at(vars(-custid), funs(./total_freq)) 
 
-#' h_bin amt ratio ----  
+#' h_bin amt ratio ------------------------------------------------------------- 
 tran %>%
   select(custid, h_bin, amt) %>%
   group_by(custid, h_bin) %>%
@@ -87,30 +88,27 @@ tran %>%
 #' 
 #' Otherwise, it means that the customer purchases are not viased at a certain time for CV to be small.... That is similar with female purchase type.
 
-library(matrixStats)
 tran %>%
   select(custid, h_bin, amt) %>%
   group_by(custid, h_bin) %>%
   summarise(freq = n()) %>%
   ungroup() %>%
   spread(h_bin, freq, fill = 0) %>%
-  mutate(time_b_mean = rowMeans(select_if(., is_numeric))) %>%
-  mutate(time_b_std = matrixStats::rowSds(as.matrix(select_if(., is_numeric)))) %>% 
-  mutate(time_b_cov = time_b_std/time_b_mean) %>%
-  select(custid, time_b_cov) -> cov_df; cov_df
-
+  mutate(time_b_mean = apply(select(., -custid), 1, mean)) %>%
+  mutate(time_b_std = apply(select(., -custid), 1, sd))
+  
 tran %>%
   select(custid, h_bin, amt) %>%
   group_by(custid, h_bin) %>%
-  summarise(sum.amt = sum(amt)) %>%
+  summarise(freq = n()) %>%
   ungroup() %>%
-  spread(h_bin, sum.amt, fill = 0) %>%
-  mutate(time_b_mean = rowMeans(select_if(., is_numeric))) %>%
-  mutate(time_b_std = matrixStats::rowSds(as.matrix(select_if(., is_numeric)))) %>% 
+  spread(h_bin, freq, fill = 0) %>%
+  mutate(time_b_mean = apply(select(., -custid), 1, mean)) %>%
+  mutate(time_b_std = apply(select(., -custid), 1, sd)) %>% 
   mutate(time_b_cov = time_b_std/time_b_mean) %>%
   select(custid, time_b_cov) -> cov_df; cov_df
 
-#' With gapminder dataset----
+#' With gapminder dataset ------------------------------------------------------
 # install.packages('gapminder')
 library(gapminder)
 data("gapminder"); glimpse(gapminder)
@@ -118,7 +116,7 @@ unique(gapminder$country)
 gapminder %>% filter(country == 'Korea, Rep.' & year==2007)
 gapminder %>% arrange(year, country)
 
-#' Basic Statistics
+#' Basic Statistics ------------------------------------------------------------
 gapminder %>%
   summarise(n_obs = n(),
             n_countries = n_distinct(country),
@@ -126,20 +124,21 @@ gapminder %>%
             med_gdpc = median(gdpPercap),
             max_gdppc = max(gdpPercap))
 
-#' Mutate new variables ----
+#' Mutate new variables ---------------------------------------------------------
 gapminder %>%
   mutate(total_gdp = pop*gdpPercap,
          le_gdp_ratio = lifeExp / gdpPercap,
          lgrk = le_gdp_ratio*100)
 
-#' Group_by ----
+#' Group_by --------------------------------------------------------------------
 gapminder %>%
   filter(year==2007) %>%
   group_by(continent) %>%
   summarise(n(), mean(lifeExp), median(lifeExp)) %>%
   arrange(-`median(lifeExp)`)
 
-#' Correlations ----
+#' Correlations ----------------------------------------------------------------
+gapminder %>% head()
 summary(gapminder)
 summary(gapminder$gdpPercap)
 cor(gapminder$gdpPercap, gapminder$lifeExp) # 0.5837062
@@ -153,81 +152,106 @@ gapminder %>%
 gapminder %>%
   ggplot(aes(log10(gdpPercap), lifeExp, col = country)) + geom_point() + theme(legend.position= "none")
 
-#' With df_imdb dataset-----
+#' With df_imdb dataset --------------------------------------------------------
 df_imdb <- read_csv('./data/imdb-5000-movie-dataset.zip'); glimpse(df_imdb)
 head(df_imdb)
 df_imdb$country <- as.factor(df_imdb$country); glimpse(df_imdb)
 
-#' Like pandas .value_count() method
+#' Like pandas .value_count() method -------------------------------------------
 df_imdb %>% group_by(country) %>% tally() %>% arrange(-n)
  
-#' America Movie Budget Distribution ----
+#' America Movie Budget Distribution -------------------------------------------
 df_imdb %>%
   filter(country == 'USA') %>%
-  ggplot(aes(budget)) + geom_histogram(bins = 50)
+  ggplot(aes(budget)) + geom_histogram(bins = 50, fill = 'steelblue')
 
-# 미국 영화 
+#' Movie Score Mean Variation --------------------------------------------------
 df_imdb %>%
   group_by(title_year) %>%
   summarise(avg_imdb_score = mean(imdb_score)) %>%
-  ggplot(aes(title_year, avg_imdb_score)) + geom_point() + geom_line()
+  ggplot(aes(title_year, avg_imdb_score)) + geom_point(col = "steelblue") + geom_line(linetype = 6)
   
-# 각 facter 별 갯수 및 percentage 확인
+#' Each Facter Cnt and Percentage -----------------------------------------------
 data(diamonds)
 diamonds %>%
   group_by(cut) %>%
   tally() %>%
   mutate(pct = round((n/sum(n))*100, 1))
 
-# 데이터에 따른 시각화
-# 1. 하나의 연속형(히스토그램)
+#' Data Visualizatoin according to data classes ---------------------------------
+#' 
+# 1. One continuous Variable...
 hist(gapminder$lifeExp)
 hist(gapminder$gdpPercap, nclass=50)
 hist(log10(gapminder$gdpPercap), nclass=50)
 
 gapminder %>%
-  ggplot(aes(gdpPercap)) + geom_histogram() + scale_x_log10()
+  ggplot(aes(gdpPercap)) + geom_histogram(bins = 50, fill = "steelblue") + scale_x_log10()
 
 gapminder %>%
   ggplot(aes(x=gdpPercap, y=lifeExp)) + geom_point() +scale_x_log10() + geom_smooth()
 
-# 2. 하나의 범주형 (막대그래프, 분할표) > 카이제곱 검정
-diamonds %>% ggplot(aes(cut)) + geom_bar() # 품질별 개수
-table(diamonds$cut) # 도수분포
+gapminder %>%
+  ggplot(aes(log10(gdpPercap), lifeExp)) + 
+  geom_point(aes(colour = factor(country))) + geom_smooth() + theme(legend.position= "none")
+
+#' 2. One factor Variable...
+diamonds %>% ggplot(aes(cut)) + geom_bar() 
+
+table(diamonds$cut)
+
 prop.table(table(diamonds$cut))
+
 round(prop.table(table(diamonds$cut)) * 100, 1)
+
+diamonds %>%
+  select(cut) %>% pull() %>% table() %>% prop.table() %>% round(2) %>% as.data.frame() %>%
+  rename(cut_type = ".") %>%
+  ggplot(aes(x= cut_type, y = Freq, fill = cut_type)) +
+  geom_bar(stat = "identity")
 
 library(MASS)
 data(survey); glimpse(survey)
 head(table(survey$W.Hnd))
-chisq.test(table(survey$W.Hnd), p=c(.3, .7)) # 특정 분포를 따르는지...
-# p< .05 이므로 특정 분포를 따르지 않는다.
+chisq.test(table(survey$W.Hnd), p=c(.3, .7)) # chisq.test to figure out it follows certains distribution...
+# p< .05, so it doesn't follow certain distribution
 
-# 3. 두 수량형 변수 (산점도)
-pairs(diamonds %>% sample_n(100))
-ggpairs(diamonds %>% sample_n(100))
+#' 3. Scatter Plot : Two more numeric variables --------------------------------
+diamonds %>% sample_n(30) %>% pairs()
+diamonds %>% sample_n(30) %>% ggpairs()
 
-# 4. 수량형 ~ 범주형 (박스플롯)
+#' 4. Numeric Variable ~ Factor Variable :: boxplot ----------------------------
 mpg %>%
   ggplot(aes(class, hwy)) + geom_boxplot()
 
+mpg %>%
+  ggplot(aes(class, hwy, color = class)) + 
+  geom_boxplot(outlier.colour = "red", outlier.shape = 8) + 
+  geom_jitter(col = 'gray', alpha = .5)
+
 mpg %>% mutate(class=reorder(class, hwy, median)); mpg$class[1:5] 
-#`class` 컬럼을 hwy의 중간값(median) 순서로 정렬하여 데이터를 정렬 
+#' reorder `class` column according to median value of `hwy`...
 
 mpg %>%
-  mutate(class=reorder(class, hwy, median)) %>% # hwy의 중간값 순서로 정렬
-  ggplot(aes(class, hwy)) + geom_jitter() + geom_boxplot(alpha=.5)
+  mutate(class=reorder(class, hwy, median)) %>%
+  ggplot(aes(class, hwy, color = class)) + 
+  geom_boxplot(outlier.colour = "red", outlier.shape = 8, alpha = .5) + 
+  geom_jitter(col = 'gray')
+
 
 mpg %>% 
   mutate(class=factor(class,
                       levels=c('2seater', 'subcompact', 'compact', 'midsize', 
-                               'minivan', 'suv', 'pickup'))) %>% ## 순서 직접 지정
-  ggplot(aes(class, hwy)) + geom_jitter() + geom_boxplot(alpha=.5)
+                               'minivan', 'suv', 'pickup'))) %>% # designate x-axis order by name...
+  ggplot(aes(class, hwy, color = class)) + 
+  geom_boxplot(outlier.colour = "red", outlier.shape = 8, alpha = .5) + 
+  geom_jitter(col = 'gray')
 
-# 4. 범주형 ~ 범주형 (분할표 / 모자이크) > 카이제곱 검정
+
+#' 4. Factor Variable ~ Factor Variable (xtabs() / mosaicplot()) > chisq.test() --------
 data("Titanic"); glimpse(Titanic)
 xtabs(~ Class + Sex + Age + Survived, data.frame(Titanic))
-xtabs(Freq ~ Class + Sex + Age + Survived, data.frame(Titanic)) # Freq를 알 수 있음
+xtabs(Freq ~ Class + Sex + Age + Survived, data.frame(Titanic)) # Freq
 
 as.tibble(fread('./data/titanic3.csv', data.table = F)) -> titanic; glimpse(titanic)
 titanic %>% replace(is.na(.), 0) -> titanic
@@ -238,15 +262,16 @@ titanic$survived <- factor(titanic$survived, levels=c(0,1), labels=c('dead', 'su
 
 glimpse(titanic)
 
-# 분할표--
+#' xtabs() ---------------------------------------------------------------------
 xtabs(survived == 'survived' ~ sex + pclass, data=titanic)
 xtabs(survived == 'survived' ~ sex + pclass, data=titanic) / xtabs(~ sex + pclass, data=titanic)
 
-# 성별과 사망률이 독립인지?
-chisq.test(xtabs(~ sex + survived, data=titanic)) # p < .05 |-> 독립이 아니다. 연관관계가 있다.
+#' chisq.test to know whether the relationship between sex and survive is independent or not...
+xtabs(~sex + survived, data = titanic)
+chisq.test(xtabs(~ sex + survived, data=titanic)) # p < .05 |-> Not independent... so there is relationship.
 
-# mosaic plot----
-mosaicplot(survived ~ pclass + sex, data = titanic, color=T) # 해석에 집중해보자
+# Mosaic plot----
+mosaicplot(survived ~ pclass + sex, data = titanic, color=T) # how to interprete the mosaic plot...
 
 
 
